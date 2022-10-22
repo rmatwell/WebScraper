bdbddc57a86ad86d5f5a0512840c4038f16da571
rmatwell <richard.atwell@hotmail.com>
2022-10-21 23:14:36 -0400

    Updated url paths

--- src/main/java/org/rmatwell/webscraper/App.java
+++ src/main/java/org/rmatwell/webscraper/App.java
@@ -3,7 +3,7 @@ package org.rmatwell.webscraper;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+
 
 /**
  * @author Richard Atwell
@@ -13,6 +13,7 @@ public class App {
     public static void main(String[] args){
         final String url =
                 "https://www.microcenter.com/search/search_results.aspx?N=4294966937&NTK=all&sortby=match&rpp=96";
+        final String rootURL = "https://www.microcenter.com";
 
         try {
             final Document document = Jsoup.connect(url).get();
@@ -23,13 +24,13 @@ public class App {
                 String name = listing.select("li.product_wrapper h2").text();
                 String sku = listing.select("p.sku").text();
                 String price = listing.select("div.price").text();
-                System.out.println(name + "  -  " + sku + "  -  " + price);
+                String relativeURL = listing.select("a.image").attr("href");
+                String absoluteURL = rootURL + "" + relativeURL;
+                System.out.println(name + "  -  " + sku + "  -  " + price + "  -  " + absoluteURL);
             }
         }
         catch (Exception ex) {
             ex.printStackTrace();
         }
-
-
     }
 }
